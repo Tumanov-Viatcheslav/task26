cb1224b3a76abeb930684b782ca2d68f9b800a0e
Tumanov Viatcheslav <tslavich@bk.ru>
2024-09-10 09:52:07 +0300

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -2,8 +2,8 @@ public class Circle {
     int x, y, radius;
     public Circle(String circleValues) {
         String[] values = circleValues.split(" ");
-        x = Integer.parseInt(values[0]);
-        y = Integer.parseInt(values[1]);
-        radius = Integer.parseInt(values[2]);
+        this.x = Integer.parseInt(values[0]);
+        this.y = Integer.parseInt(values[1]);
+        this.radius = Integer.parseInt(values[2]);
     }
 }
