dabbc58b41520625c65ac360d90d37b1a85731b9
Tumanov Viatcheslav <tslavich@bk.ru>
2024-09-10 10:28:59 +0300

    Update Circles.java

--- src/Circles.java
+++ src/Circles.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class Circles {
     public static void main(String[] args) {
-        boolean isIntersected;
+        boolean isIntersectedOutside, isIntersectedInside;
         int character;
         String buffer = "";
         String[] circlesValues;
@@ -19,10 +19,13 @@ public class Circles {
         }
         circlesValues = buffer.split("\r\n");
         Circle circle1 = new Circle(circlesValues[0]), circle2 = new Circle(circlesValues[1]);
-        isIntersected = ((circle2.x - circle1.x) * (circle2.x - circle1.x) + (circle2.y - circle1.y) * (circle2.y - circle1.y)) <=
+        isIntersectedOutside = ((circle2.x - circle1.x) * (circle2.x - circle1.x) + (circle2.y - circle1.y) * (circle2.y - circle1.y)) <=
                             (circle1.radius + circle2.radius) * (circle1.radius + circle2.radius);
+        isIntersectedInside = ((circle2.x - circle1.x) * (circle2.x - circle1.x) + (circle2.y - circle1.y) * (circle2.y - circle1.y)) <=
+                            (circle2.radius - circle1.radius) * (circle2.radius - circle1.radius);
+
         try(FileWriter output = new FileWriter("output.txt")) {
-            if (isIntersected) {
+            if (isIntersectedOutside && isIntersecredInside) {
                 output.write("YES");
             }
             else {
